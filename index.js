9c230805bcdd991af32668a048b73eea694ebafc
Aaron Tucker <atucker@solution-tek.com>
2023-02-22 09:26:50 -0500

    v1.0.0

---
+++ index.js
@@ -0,0 +1,27 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const jiraPrefixes = core.getInput("jira-prefixes");
+
+async function run() {
+    try {
+        const prTitle = github.context.payload.pull_request.title;
+        const prefixesArray = jiraPrefixes.split(',').trim();
+        let keyFound = false
+        prefixesArray.forEach(prefix => {
+            let regex = new RegExp(`${prefix}-[0-9]+`);
+            if (regex.test(prTitle)) {
+                keyFound = true
+            }
+        })
+
+        if (!keyFound) {
+            core.setFailed("Jira Issue Key missing in pull request title.");
+            return;
+        }
+    } catch (error) {
+        core.info(error);
+    }
+}
+
+run();
